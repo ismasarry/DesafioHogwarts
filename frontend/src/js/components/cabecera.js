229e813f62d71d2964e1a9dfb45f9b6e54bd2c5c
Raul GM <loscanos3raul@gmail.com>
2024-11-08 09:38:01 +0100

    Perfil
    Añadido el perfil para que se pueda editar (no funciona todavía a espera del sessionStorage)

--- frontend/src/js/components/cabecera.js
+++ frontend/src/js/components/cabecera.js
@@ -1,12 +1,14 @@
+//Raul Gutierrez
 const headerContainer = document.getElementById("header-container")
 
 const usuario = "Juan"//sessionStorage.getItem("usuario")
+const usuarioCorreo = "CorreoPrueba@gmail.com"
 
 headerContainer.innerHTML = `
   <!-- Cabecera -->
   <header class="header p-3 d-flex align-items-center justify-content-between shadow" style="background-color: #007bff; color: white;">
     <div class="d-flex align-items-center">
-      <span class="h4 mb-0 me-3 text-white">Dashboard</span>
+      <span class="h4 mb-0 me-3 text-white">Hogarwts</span>
       <button class="btn" style="background-color: #0056b3; color: white;">Ranking de Casas</button>
     </div>
     
@@ -29,7 +31,39 @@ headerContainer.innerHTML = `
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
         </div>
         <div class="modal-body">
-          <!--Aqui vendrá el formulario del perfil-->
+
+        <form id="perfilUsuarioForm">
+          <div class="col-md-6">
+            <label for="nombre" class="form-label">Nombre:</label>
+            <input class="form-control" type="text" id="nombre" name="nombre" required>
+          </div>
+
+          <div class="col-md-6">
+            <label class="form-label" for="correo">Correo Electrónico:</label>
+            <input class="form-control" type="email" id="correo" name="correo" required>
+          </div>
+
+          <div class="col-md-6">
+            <label for="contrasena" class="form-label">Contraseña:</label>
+            <input class="form-control" type="contrasena" id="contrasena" name="contrasena" required>
+          </div>
+
+          <div class="col-md-6">
+            <label for="casa" class="form-label">Casa de Harry Potter:</label>
+            <select id="casa" name="casa" class="form-select" required>
+              <option value="gryffindor">Gryffindor</option>
+              <option value="hufflepuff">Hufflepuff</option>
+              <option value="ravenclaw">Ravenclaw</option>
+              <option value="slytherin">Slytherin</option>
+            </select>
+          </div>
+
+          <div class="col-md-12">
+            <label for="foto" class="form-label">Foto:</label>
+            <input class="form-control" type="file" id="foto" name="foto" accept="image/*">
+          </div>
+
+        </form>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-primary" data-bs-dismiss="modal" id="guardarCambios">Guardar cambios</button>
@@ -43,16 +77,21 @@ headerContainer.innerHTML = `
 `;
 
 document.getElementById("botonPerfil").addEventListener("click", () => {
-    const modalPerfil = new bootstrap.Modal(document.getElementById('modalPerfil'))
-    modalPerfil.show()
+
+  document.getElementById("nombre").value = usuario //usuario.nombre
+  document.getElementById("correo").value = usuarioCorreo //usuario.gmail
+  //falta la foto y la casa
+
+  const modalPerfil = new bootstrap.Modal(document.getElementById('modalPerfil'))
+  modalPerfil.show()
 })
 
 document.getElementById("guardarCambios").addEventListener("click", () => {
-    //Editar el usuario
+  //Editar el usuario
 })
 
 document.getElementById("botonCerrarSesion").addEventListener("click", () => {
-    //sessionStorage.clear()
-    console.log("sesion cerrada")
-    window.location.href = "./../index.html"
+  //sessionStorage.clear()
+  console.log("sesion cerrada")
+  window.location.href = "./../index.html"
 })
