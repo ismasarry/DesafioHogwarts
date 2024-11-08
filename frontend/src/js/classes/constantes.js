3ab34db1a16963d25fc387e90cd87299344c20dd
Raul GM <loscanos3raul@gmail.com>
2024-11-08 10:40:20 +0100

    Preparación del inicio

---
+++ frontend/src/js/classes/constantes.js
@@ -0,0 +1,7 @@
+export const tablaNivel = [
+    { nivel: 1, minExp: 0, maxExp: 50 },
+    { nivel: 2, minExp: 50, maxExp: 150 },
+    { nivel: 3, minExp: 150, maxExp: 300 },
+    { nivel: 4, minExp: 300, maxExp: 500 },
+    { nivel: 5, minExp: 500, maxExp: Infinity }
+]
---
+++ frontend/src/js/ui/inicioUI.js
@@ -0,0 +1,33 @@
+//Raul Gutierrez
+import { tablaNivel } from "../classes/constantes.js"
+
+const mainImage = document.getElementById("mainImage")
+const dynamicText = document.getElementById("dynamicText")
+const progressBar = document.getElementById("progressBar")
+
+const usuario = parseInt(sessionStorage.getItem("usuario"))
+
+let niveles = tablaNivel.find(nivel => usuario.nivel >= nivel.minExp && usuario.exp < nivel.maxExp)
+
+let nivelMinExp = niveles.minExp
+let nevelMaxExp = niveles.maxExp
+let progressInLevel = ((usuario.exp - nivelMinExp) / (nevelMaxExp - nivelMinExp)) * 100
+
+progressBar.style.width = `${progressInLevel}%`
+progressBar.setAttribute("aria-valuenow", progressInLevel)
+progressBar.textContent = `Nivel ${usuario.nivel} - ${progressInLevel.toFixed(1)}%`
+
+let img = "./../assets/Gryffindor.png"
+
+/*if (usuario.foto == "Gryffindor.png") {
+    img = "./../assets/Gryffindor.png"
+}else if(usuario.foto == "Hufflepuff.png"){
+    img = "./../assets/Hufflepuff.png"
+}else if(usuario.foto == "Ravenclaw.png"){
+    img = "./../assets/Ravenclaw.png"
+}else if(usuario.foto == "Slytherin.png"){
+    img = "./../assets/Slytherin.png"
+}*/
+
+mainImage.src = img
+dynamicText.textContent = "¡Bienvenido al dashboard! Aquí puedes ver tu progreso y nivel actuales."
