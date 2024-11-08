aacbb65eb404057344a7e199fabbc49107075739
Jaime <jaime.ornu@gmail.com>
2024-11-08 06:24:00 +0100

    Modelo, CRUD, factory, migration y seeder de Casa

---
+++ backend/routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\casaController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+//Jaime Ortega
+Route::middleware(['auth:sactum'])->group(function () {
+    Route::prefix('casa')->group(function () {
+        Route::get('/', [casaController::class, 'getAllCasas']);
+        Route::get('/{id}', [casaController::class, 'getCasa']);
+        Route::get('/integrantes/{id}', [casaController::class, 'getUsuariosCasa']);
+        Route::post('/', [casaController::class, 'createCasa']);
+        Route::put('/{id}', [casaController::class, 'updateCasa']);
+        Route::delete('/{id}', [casaController::class, 'deleteCasa']);
+    });
+});
