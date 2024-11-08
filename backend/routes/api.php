ccb0238c24b5ac75f90c1bc255913d330c1afd97
Ismael <145430109+ismasarry@users.noreply.github.com>
2024-11-08 09:49:55 +0100

    añado nombres

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,18 +1,16 @@
 <?php
-//Ismael Sarrion
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\usuarioRolController;
 use Illuminate\Http\Request;
 use App\Http\Controllers\UsuarioController;
 
+//Ismael Sarrion
+
 Route::get('/usuarioRoles', [usuarioRolController::class, 'getTodosUsuarioRoles']);
-
 Route::get('/usuarioRoles/{id}', [usuarioRolController::class, 'getUsuarioRolPorId']);
-
 Route::post('/usuarioRoles', [usuarioRolController::class, 'postUsuarioRol']);
-
 Route::put('/usuarioRoles/{id}', [usuarioRolController::class, 'putUsuarioRol']);
-
 Route::delete('/usuarioRoles/{id}', [usuarioRolController::class, 'deleteUsuarioRol']);
 
 //Raul Gutierrez
