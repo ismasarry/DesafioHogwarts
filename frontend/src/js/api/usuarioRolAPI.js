b60ebebf7dfcb22c976745428b64a8781a1a812f
Jaime <jaime.ornu@gmail.com>
2024-11-19 09:57:01 +0100

    Creación usuarioRolAPI (lo necesario), añadidas rutas en constantes, creación sideBarAdmin, y cambio de orden el sideBarProfesor

---
+++ frontend/src/js/api/usuarioRolAPI.js
@@ -0,0 +1,29 @@
+//Jaime Ortega
+import { constantes } from "../classes/constantes"
+
+export async function mostrarRolesUsuario($idUsuario) {
+    const rutaUsuario = constantes.urlApi + constantes.usuRol + $idUsuario
+
+    try {
+        const respuesta = await fetch(rutaUsuario, {
+            method: 'GET',
+            headers: {
+                "Authorization": "Bearer " + sessionStorage.getItem("token"),
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*"
+            }
+        
+        })
+
+        if (!respuesta.ok) {
+            throw new Error(`Error al obtener la lista de roles del usuario. Código de estado: ${respuesta.status}`);
+        }
+
+        const resultado = await respuesta.json()
+        return resultado
+
+    } catch (error) {
+        console.error('Error en la función getTodosUsuarios:', error.message);
+        throw error;
+    }
+}
