d24d41351073c647db41108aeecfd3134db8fc36
Raul GM <loscanos3raul@gmail.com>
2024-11-26 08:50:39 +0100

    mensaje duelo

--- backend/app/Http/Controllers/gmailController.php
+++ backend/app/Http/Controllers/gmailController.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Http\Controllers;
 //isamel sarrion
+
+use App\Models\hechizos;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Models\Usuario; 
@@ -40,6 +42,50 @@ class gmailController extends Controller
                 $message->to($request->gmail)
                         ->subject('Recuperación de Contraseña');
                 $message->from('welcometohogwartslittlemuggle@gmail.com', 'Recupera Tu Contraseña');
+            });
+
+            return response()->json([
+                'enviado' => true,
+                'mensaje' => 'Correo de recuperación enviado con éxito.',
+            ], 200);
+        } catch (Exception $e) {
+            
+            return response()->json([
+                'enviado' => false,
+                'mensaje' => 'Error en el servidor: ' . $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    //Raul Gutierrez
+    public function dueloCorreo(Request $request)
+    {
+        try {
+            $request->validate([
+                'gmail' => 'required|email',
+            ]);
+
+            $usuario = Usuario::where('gmail', $request->gmail)->first();
+            $ganador = hechizos::where(); //Gandor del duelo 
+            
+            if (!$usuario) {
+                return response()->json([
+                    'enviado' => false,
+                    'mensaje' => 'No se encontró un usuario con ese correo.',
+                ], 404);
+            } 
+
+            $datos = [
+                'nombreUsuario' => $usuario->nombre, 
+                'gmail' => $request->gmail,
+                'ganador' => $ganador->ganador,
+                'historial' => $ganador->historial
+            ];
+
+            Mail::send('notificacionDuelo', $datos, function($message) use ($request) {
+                $message->to($request->gmail)
+                        ->subject('Duelo');
+                $message->from('welcometohogwartslittlemuggle@gmail.com', 'Has realizado un duelo');
             });
 
             return response()->json([
---
+++ backend/resources/views/notificacionDuelo.blade.php
@@ -0,0 +1,21 @@
+<!--Raúl Gutierrez-->
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Duelo</title>
+</head>
+<body>
+    <h2>Hola {{ $nombreUsuario }},</h2>
+    <p>Recibimos una solicitud para recuperar tu contraseña. Hemos generado una nueva contraseña para ti:</p>
+    
+    <h3>Tu nueva contraseña es: <strong>{{ $nuevaContrasena }}</strong></h3>
+    
+    <p>Por favor, utilízala para iniciar sesión en tu cuenta.</p>
+    <p>Si solicitaste este cambio de contraseña y quieres volver a cambiarla puedes hacerlo desde tu perfil</p>
+    <p>Si no solicitaste este cambio de contraseña, por favor ignora vuelve a cambiarla desde tu perfil.</p>
+
+    <p>Atentamente,<br>El equipo de Hogwarts</p>
+</body>
+</html>
